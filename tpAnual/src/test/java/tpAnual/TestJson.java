73f39c190929144603ed9ef53c0803c52cfc5131
Fiona Lewis <fionalewisx@gmail.com>
2018-04-20 01:59:21 -0300

    Subo los jsons a mi me funcionan local pero para que vean ustedes, sino vamos a tener que ver como implementarlo

--- tpAnual/src/test/java/tpAnual/TestJson.java
+++ tpAnual/src/test/java/tpAnual/TestJson.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class TestJson {
 	@Test
 	public void testJsonTraeClientes() throws FileNotFoundException, InstantiationException, IllegalAccessException{
-		List<Cliente> clientes = DAOJson.deserializarLista(Cliente.class, "//home//dds//Desktop//JSONs//jsonClientes.json");//(RepoJson.cargarArchivoClientes("//home//dds//Desktop//JSONs//jsonCliente.json"));
+		List<Cliente> clientes = DAOJson.deserializarLista(Cliente.class, "//home//dds//git//TpAnualDds//tpAnual//JSONs//jsonClientes.json");
 		Assert.assertEquals("homero", clientes.get(1).nombre);
 		Assert.assertEquals(2, clientes.size());
     }
@@ -17,20 +17,20 @@ public class TestJson {
     @Test
 	public void testJsonTraeUnCliente() throws FileNotFoundException, InstantiationException, IllegalAccessException{
     	
-    	Cliente cli = (Cliente) DAOJson.buscarIndexEnLista(Cliente.class, 1,"//home//dds//Desktop//JSONs//jsonClientes.json");
+    	Cliente cli = (Cliente) DAOJson.buscarIndexEnLista(Cliente.class, 1,"//home//dds//git//TpAnualDds//tpAnual//JSONs//jsonClientes.json");
     	Assert.assertEquals("homero",cli.nombre);
     	Assert.assertEquals("homero",cli.getNombre());
     }
     
     @Test
     public void testJsonTraeUnDispositivo() throws FileNotFoundException, InstantiationException, IllegalAccessException{
-    	Dispositivo disp = (Dispositivo) DAOJson.buscarIndexEnLista(Dispositivo.class, 1,"//home//dds//Desktop//JSONs//jsonDispositivos.json");
+    	Dispositivo disp = (Dispositivo) DAOJson.buscarIndexEnLista(Dispositivo.class, 1,"//home//dds//git//TpAnualDds//tpAnual//JSONs//jsonDispositivos.json");
     	Assert.assertEquals("microondas", disp.getNombreDisp());
     }
     
     @Test
     public void testJsonTraeUnAdmin() throws FileNotFoundException, InstantiationException, IllegalAccessException {
-    	Administrador admin = (Administrador) DAOJson.buscarIndexEnLista(Administrador.class, 0,"//home//dds//Desktop//JSONs//jsonAdministradores.json");
+    	Administrador admin = (Administrador) DAOJson.buscarIndexEnLista(Administrador.class, 0,"//home//dds//git//TpAnualDds//tpAnual//JSONs//jsonAdministradores.json");
     	Assert.assertEquals(20000, admin.getCodAdmin());
     }
 }
