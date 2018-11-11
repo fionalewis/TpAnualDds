36753fc078d587d21fea3c53b292b11b2a06ffd9
nacho <nachoperez714@gmail.com>
2018-11-11 17:23:39 -0300

    fix homecontroller

--- tpAnual/src/main/java/controllers/HomeController.java
+++ tpAnual/src/main/java/controllers/HomeController.java
@@ -64,10 +64,10 @@ public class HomeController implements WithGlobalEntityManager, TransactionalOps
 		{
 		//TODO ir a buscar a la base de datos al cliente posta
 		//Cliente cliente = ClienteFactory.getCliente(nroDoc);
-		//user = ClienteFactory.getCliente(username);
+		user = ClienteFactory.getCliente(username);
 		//user = ClienteRepository.obtenerCliente(username);
-			user.setUserName("user");
-			user.setPassword("pass");
+		//	user.setUserName("user");
+		//	user.setPassword("pass");
 		}
 		catch (NoResultException e)
 		{
