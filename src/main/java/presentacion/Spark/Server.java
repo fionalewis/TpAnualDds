b81c7a1475acd1bcb194744cd834d87b5e5f736b
flewis <flewis@BFNB13397.baunet.local>
2018-12-12 18:22:34 -0300

    ptm

--- src/main/java/presentacion/Spark/Server.java
+++ src/main/java/presentacion/Spark/Server.java
@@ -5,7 +5,7 @@ import spark.debug.DebugScreen;
 
 public class Server {
 	public static void main(String[] args) {
-		Spark.port(9001);//Integer.valueOf(System.getenv("PORT")));//
+		Spark.port(Integer.valueOf(System.getenv("PORT")));//9001);
 		DebugScreen.enableDebugScreen();
 		Router.configure();
 	}
