b4161054dd00a30de16d977fadc2532a8b0349b2
salometredici <salome.tredici@outlook.com>
2018-12-18 22:05:00 -0300

    merge

--- src/main/java/db/Runner.java
+++ src/main/java/db/Runner.java
@@ -16,22 +16,14 @@ import modelo.Actuador.Actuador;
 import modelo.Reglas.CondicionSensorYValor;
 import modelo.Reglas.Regla;
 import modelo.deviceState.Encendido;
-<<<<<<< HEAD
-=======
 import modelo.devices.IntervaloDispositivo.modo;
->>>>>>> fionaheroku
 import modelo.devices.*;
-import modelo.devices.IntervaloDispositivo.modo;
+import modelo.devices.Sensor;
 import modelo.repositories.*;
 import modelo.geoLocation.Transformador;
 import modelo.geoLocation.Zona;
-<<<<<<< HEAD
-import modelo.repositories.*;
-import modelo.factories.CategoriaFactory;
-=======
 import modelo.devices.DispositivoEstandar;
 import modelo.devices.DispositivoInteligente;
->>>>>>> fionaheroku
 import modelo.factories.DispositivoFactory;
 import modelo.users.Administrador;
 import modelo.users.Categoria;
@@ -68,23 +60,17 @@ public class Runner implements WithGlobalEntityManager, EntityManagerOps, Transa
 		/* con estas borras lo que haya antes en la tabla para que te quede de cero lo de los jsons
 		 drop table if exists tp_anual_dds.actuador;
 		 drop table if exists tp_anual_dds.administrador;
-
 		 drop table if exists tp_anual_dds.intervalodispositivo;
 		 drop table if exists tp_anual_dds.reporte;
 		 drop table if exists tp_anual_dds.dispositivo;
-
 		 drop table if exists tp_anual_dds.cliente;
 		 drop table if exists tp_anual_dds.transformador;
-
 		 drop table if exists tp_anual_dds.condicion;
 		 drop table if exists tp_anual_dds.registro_mediciones;
-
-
 		 drop table if exists tp_anual_dds.categoria;
 		 drop table if exists tp_anual_dds.condicion_dos_sensores;
 		 drop table if exists tp_anual_dds.condicion_sensor_y_valor;
 		 drop table if exists tp_anual_dds.regla;
-
 		 drop table if exists tp_anual_dds.sensor;
 		drop table if exists tp_anual_dds.zona;
 		*/
@@ -109,13 +95,9 @@ public class Runner implements WithGlobalEntityManager, EntityManagerOps, Transa
 			setClientesCompletos();
 			System.out.println("Clientes - OK");
 			setUnDispositivoANico();
-<<<<<<< HEAD
-			setUpSuenio();
-=======
 			System.out.println("Nico - OK");
 			setUpSuenio();
 			System.out.println("Suenio - OK");
->>>>>>> fionaheroku
 			//setUnDispositivoACliente();
 			//setSensor();
 			//setRegla();
@@ -249,11 +231,7 @@ public class Runner implements WithGlobalEntityManager, EntityManagerOps, Transa
 	public void setUpSuenio(){
 		
 		DispositivoInteligente pepa = new DispositivoInteligente("Aire Acondicionado",1.5,2017,11,22,0,0,0,true);
-<<<<<<< HEAD
-		DispositivoInteligente est = new DispositivoInteligente("Aire Acondicionado",1.3,2018,11,22,0,0,0,true);
-=======
 		DispositivoInteligente est = new DispositivoInteligente("Pepa",1.3,2018,11,22,0,0,0,true);
->>>>>>> fionaheroku
 		LocalDateTime fecha = LocalDateTime.of(2018,2,21,0,0,0);
 		pepa.ahorroEnergia(fecha);
 		LocalDateTime fechaap = LocalDateTime.of(2018,5,2,0,0,0);
@@ -261,32 +239,19 @@ public class Runner implements WithGlobalEntityManager, EntityManagerOps, Transa
 		LocalDateTime fechae = LocalDateTime.of(2018,7,24,0,0,0);
 		pepa.encender(fechae);
 		LocalDateTime fechar = LocalDateTime.of(2018,9,11,0,0,0);
-<<<<<<< HEAD
-		pepa.apagar(fechar);
-=======
 		//pepa.apagar(fechar);
->>>>>>> fionaheroku
 		/*((DispositivoInteligente) d1).encender(LocalDateTime.of(2018,8,21,4,0,0));
 		((DispositivoInteligente) d1).apagar(LocalDateTime.of(2018,8,21,5,0,0));
 		((DispositivoInteligente) d1).ahorroEnergia(LocalDateTime.of(2018,8,21,6,0,0));
     	((DispositivoInteligente) d1).apagar(LocalDateTime.of(2018,8,21,8,30,0));*/
 		List<Dispositivo> disp = new ArrayList<>();disp.add(pepa);disp.add(est);
 		Cliente c = new Cliente("pepa","pig","pepi","pik",2018,8,21,TipoDocumento.DNI,"40403568","12345678","pppp 986",disp);
-<<<<<<< HEAD
-
-		ClienteRepository.addClienteConDispositivosEIntervalos(c);
-	}
-	
-	
-	//
-=======
 		
 		
 		
 		ClienteRepository.addClienteConDispositivosEIntervalos(c);
 	
 	}
->>>>>>> fionaheroku
 
 	public void setUpTransformadores(){
 		try{ 
